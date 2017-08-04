be3d2ed5e0bbbc6eada8b4762e7f51a0da8d8c57
fpf <fengpengfei@bilibili.com>
2017-08-04 12:26:48 +0800

    去掉欢迎界面

--- App/controllers/Welcome.php
+++ App/controllers/Welcome.php
@@ -17,7 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function index() {
-		$this -> load -> view('welcome.html');
+        redirect('Pkadmin/Login/index');
+		//$this -> load -> view('welcome.html');
 	}
 
 }
